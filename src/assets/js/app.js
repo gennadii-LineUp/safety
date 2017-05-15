4d237f8ca02b2e81ad6edb6c373e7f22c3e22754
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-05-15 14:36:41 +0300

    added layout +nav.mobile for admin-->client

---
+++ src/assets/js/app.js
@@ -0,0 +1,39 @@
+/* MOBILE MENU */
+var menuSlide = function() {
+	$('#nav-icon1').click(function(){
+		$(this).toggleClass('open');
+	});
+    $('#nav-icon1').click(function() {
+    	$('.sidebar-nav').slideToggle(400);
+    });
+    $(window).resize(function(){
+		if ($(window).width() > 991) {
+			$(".sidebar-nav").slideDown();
+        }
+  	    else {
+  			$("#nav-icon1").removeClass('open');	
+            $(".sidebar-nav").slideUp();	    	
+  	    }
+    });
+};
+$(document).ready(menuSlide);
+
+/* MOBILE TABLES */
+(function () {
+       var headertext = [],
+         headers = document.querySelectorAll("th"),
+         tablerows = document.querySelectorAll("th"),
+         tablebody = document.querySelector("tbody");
+
+         for(var i = 0; i < headers.length; i++) {
+           var current = headers[i];
+           headertext.push(current.textContent.replace(/\r?\n|\r/,""));
+         } 
+         for (var i = 0, row; row = tablebody.rows[i]; i++) {
+           for (var j = 0, col; col = row.cells[j]; j++) {
+             col.setAttribute("data-th", headertext[j]);
+           } 
+         }
+} ());	
+
+	
