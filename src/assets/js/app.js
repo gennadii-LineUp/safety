3ede8e3004cabfc137901e61496483230f4b43c6
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-05-17 14:48:55 +0300

    fixed error message from app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -25,16 +25,17 @@ $(document).ready(menuSlide);
         headers = document.querySelectorAll("th"),
         tablerows = document.querySelectorAll("th"),
         tablebody = document.querySelector("tbody");
-
-    for(var i = 0; i < headers.length; i++) {
-        var current = headers[i];
-        headertext.push(current.textContent.replace(/\r?\n|\r/,""));
-    }
-    for (var i = 0, row; row = tablebody.rows[i]; i++) {
-        for (var j = 0, col; col = row.cells[j]; j++) {
-            col.setAttribute("data-th", headertext[j]);
+    if (document.querySelector("table")) {
+        for(var i = 0; i < headers.length; i++) {
+            var current = headers[i];
+            headertext.push(current.textContent.replace(/\r?\n|\r/,""));
+        }
+        for (var i = 0, row; row = tablebody.rows[i]; i++) {
+            for (var j = 0, col; col = row.cells[j]; j++) {
+                col.setAttribute("data-th", headertext[j]);
+            }
         }
     }
-} ());	
+} ());
 
 	
