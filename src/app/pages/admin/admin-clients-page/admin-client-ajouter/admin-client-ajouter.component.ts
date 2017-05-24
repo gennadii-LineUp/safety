fda0572d93c31ac6af65ce13d48620af8e4c6a3f
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-05-24 12:57:47 +0300

    admin-->client-->ajouter fixed checkbox and input's caption

--- src/app/pages/admin/admin-clients-page/admin-client-ajouter/admin-client-ajouter.component.ts
+++ src/app/pages/admin/admin-clients-page/admin-client-ajouter/admin-client-ajouter.component.ts
@@ -31,12 +31,25 @@ export class Client{
 export class AdminClientAjouterComponent implements OnInit {
     loading = false;
     error = '';
+    billingAddressIsDifferent:boolean = true;
 
     client: Client[] = [];
 
     constructor(private router: Router) {}
 
-    ngOnInit() {  }
+    ngOnInit() {
+        // var spins = document.getElementsByClassName("spin");
+        // for (var i = 0, len = spins.length; i < len; i++) {
+        //     var spin = spins[i],
+        //         span = spin.getElementsByTagName("span"),
+        //         input = spin.getElementsByTagName("input")[0];
+        //
+        //     input.onchange = function() { input.value = +input.value || 0; };
+        //     span[1].onclick = function() { input.value = Math.max(0, +input.value - 1); };
+        //     span[0].onclick = function() { input.value -= -1; };
+        // }
+
+    }
 
     private cancellError() {
         this.loading = false;
@@ -61,8 +74,24 @@ export class AdminClientAjouterComponent implements OnInit {
                             employeesLimit: number) {
         this.loading = true;
         console.log(email, company, address, postalCode, city, billingAddressIfDifferent, diffName, diffAddress, diffPostalCode, diffCity, phone, numberSiret, contactName, contactPhone, contactEmail, employeesLimit);
-        //this.router.navigate(['/admin/client']);
+
 
     }
+
+    public gotoAdminClientForm() {
+        this.router.navigate(['/admin/client']);
+    }
+
+    public toggleDifferentBillingAddress(checkbox) {
+        this.billingAddressIsDifferent = !checkbox.target.checked;
+    }
+
+    public increaseEmployees() {
+        console.log('+');
+    }
+    public decreaseEmployees() {
+        console.log('-');
+    }
+
 
 }
