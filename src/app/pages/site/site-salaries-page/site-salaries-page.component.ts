d72f150d4e667b0010f75263992b3cabef90f0d9
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-06-14 12:39:13 +0300

    site-->salaries-->creation-etap2 layout finished

--- src/app/pages/site/site-salaries-page/site-salaries-page.component.ts
+++ src/app/pages/site/site-salaries-page/site-salaries-page.component.ts
@@ -18,10 +18,29 @@ export class SiteSalariesPageComponent implements OnInit {
                 private router: Router) { }
 
   ngOnInit() {
+      this.tableMobileViewInit();
   }
 
     gotoNewSalariesForm() {
         this.router.navigate(['/site/salaries/ajouter-un-salaries-etap1']);
+    }
+
+    public tableMobileViewInit() {
+        let headertext = [],
+            headers = document.querySelectorAll("th"),
+            tablerows = document.querySelectorAll("th"),
+            tablebody = document.querySelector("tbody");
+        if (document.querySelector("table")) {
+            for(let i = 0; i < headers.length; i++) {
+                let current = headers[i];
+                headertext.push(current.textContent.replace(/\r?\n|\r/,""));
+            }
+            for (let i = 0, row; row = tablebody.rows[i]; i++) {
+                for (let j = 0, col; col = row.cells[j]; j++) {
+                    col.setAttribute("data-th", headertext[j]);
+                }
+            }
+        }
     }
 
 
