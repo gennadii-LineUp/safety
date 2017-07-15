a536e0f80963cc950af6aea9d0bed9560d96856b
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-07-15 12:14:35 +0300

    site-->accueil  finished

--- src/app/pages/site/site-accueil-page/site-accueil-page.component.ts
+++ src/app/pages/site/site-accueil-page/site-accueil-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {SiteService} from '../../../services/site/site.service';
 import {DataService} from '../../../services/DataService.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import {ErrorMessageHandlerService} from '../../../services/error/error-message-handler.service';
 
 @Component({
   selector: 'site-accueil-page',
@@ -9,38 +10,65 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./site-accueil-page.component.css'],
     providers: [SiteService]
 })
-export class SiteAccueilPageComponent implements OnInit {
-    loading: boolean = false;
-    loadingSalarieUsed: boolean = false;
-    loaded: boolean = false;
-    loadedSalarieUsed: boolean = false;
-    errorLoad: string = '';
-    errorSalaries: string = '';
-    errorCreating: string = '';
-    successCreating: string = '';
+export class SiteAccueilPageComponent implements OnInit, OnDestroy {
+    loading = true;
+    errorLoad = '';
+    imgServer: any;
+    showImg: true;
 
     id_site: number;
+    siteName: string;
 
 
-   constructor(private dataService: DataService) {}
+   constructor(private errorMessageHandlerService: ErrorMessageHandlerService,
+               private siteService: SiteService) {}
 
     ngOnInit() {
         this.id_site = localStorage.id_site;
         window.document.querySelectorAll('ul li:first-child')['0'].classList.add('active');
-
-        // this.id_site = this.siteService.getIdSite();       // doesn't work
-
-        //console.log('from component');                      // doesn't work
-        // this.dataService.getData().subscribe(data => {     // doesn't work
-        //     this.id_site = data;
-        // })
-        console.log('get from LS ' + this.id_site);
+        this.getAccueilInfoFunction();
+        this.getFromServerAccueilImageFunction();
    }
 
     ngOnDestroy() {
         window.document.querySelectorAll('ul li:first-child')['0'].classList.remove('active');
     }
 
+  public getAccueilInfoFunction() {
+    this.siteService.getAccueilInfo(this.id_site)
+      .subscribe(result => {
+        if (result) {
+          this.loading = false;
+          console.log(result);
+          this.siteName = result.name;
+        }
+      }, (err) => {
+        this.loading = false;
+        console.log(err);
+        this.errorLoad = this.errorMessageHandlerService.checkErrorStatus(err);
+      });
+  }
+
+
+  public getFromServerAccueilImageFunction() {
+    this.loading = true;
+    this.siteService.getFromServerAccueilImage(this.id_site)
+      .subscribe(result => {
+        if (result) {
+          this.loading = false;
+          this.showImg = true;
+          console.log(result);
+          const src = 'data:' + result.contentType + ';base64,';
+          console.log(src);
+          this.imgServer = src + result.content;
+
+        }
+      }, (err) => {
+        this.loading = false;
+        console.log(err);
+        this.errorLoad = this.errorMessageHandlerService.checkErrorStatus(err);
+      });
+  }
 
 
 }
