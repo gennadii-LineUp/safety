263d797e858064e01eebb953f724b273608ec74b
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-07-24 14:13:22 +0300

    site-->reglages finished

--- src/app/pages/site/site-reglages-page/responsable/responsable.component.ts
+++ src/app/pages/site/site-reglages-page/responsable/responsable.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {TableSortService} from '../../../../services/table-sort.service';
 import {SiteService} from '../../../../services/site/site.service';
 import {ErrorMessageHandlerService} from '../../../../services/error/error-message-handler.service';
+import {EmployeesSettingAccessClass} from '../../../../models/const/employee-setting-access-class';
 
 @Component({
   selector: 'responsable-site',
@@ -22,7 +23,12 @@ export class ResponsableSiteComponent implements OnInit {
 
     id_site: number;
 
-    employee_search = [];
+    employee_fromSearch = [];
+    employee_forAccess = [];
+    types_employeeAccess = [
+      { id: 1,  description: 'acces general, access to the employees and park of the machines of the site' },
+      { id: 0,  description: 'acces technical, access to machinery park of a site' }
+    ];
 
     headers: any[] = [
         { display: 'Nom du salarié', variable: 'name',    filter: 'text' },
@@ -56,7 +62,7 @@ export class ResponsableSiteComponent implements OnInit {
         if (result) {
           this.loading = false;
           console.log(result);
-          this.employee_search = result.items;
+          this.employee_fromSearch = result.items;
           this.loaded = true;
           setTimeout(() => {
             this.siteService.tableMobileViewInit();
@@ -69,9 +75,51 @@ export class ResponsableSiteComponent implements OnInit {
       });
   }
 
+  public takeEmployee_forSettingAccessFunction(employee) {
+      console.log(employee);
+      this.emptyTableMobile = false;
+      const employee_forSettingAccess = new EmployeesSettingAccessClass(employee.id, employee.name, employee.surname, employee.numSecu, 0);
+
+      this.employee_forAccess.push(employee_forSettingAccess);
+      for (let i = 0; i < this.employee_fromSearch.length; i++) {
+          if (+this.employee_fromSearch[i].id === +employee.id) {
+            this.employee_fromSearch.splice(i, 1);
+            break;
+          }
+      }
+  }
+
+  public submitForm() {
+      console.log(this.employee_forAccess);
+      this.cancellMessages();
+      this.loading = true;
+
+      // const visites = new VisitesClass(_datepicker_medicalVisit, _datepicker_caces);
+      // console.log(visites);
+      this.siteService.addEmployeeAccess(this.employee_forAccess, this.id_site)
+        .subscribe(result => {
+          if (result) {
+            this.loading = false;
+            console.log(result);
+           // this.successUpdate = 'Bravo! Vos modifications sont enregistrées.';
+            setTimeout(() => {
+              this.siteService.tableMobileViewInit();
+            }, 100);
+          }
+        }, (err) => {
+          this.loading = false;
+          console.log(err);
+          this.errorLoad = this.errorMessageHandlerService.checkErrorStatus(err);
+        });
+
+
+   }
+
   public setEmptyData() {
     this.loaded = false;
     this.cancellMessages();
+    this.employee_fromSearch = [];
+    this.employee_forAccess = [];
     return true;
   }
   private cancellMessages() {
--- src/app/services/pipes/capitalize.pipe.ts
+++ src/app/services/pipes/capitalize.pipe.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'capitalize'
+  name: 'capitalizeFirstLetter'
 })
 export class CapitalizePipe implements PipeTransform {
 
   transform(value: any) {
     if (value) {
-      return value.charAt(0).toUpperCase() + value.slice(1);
+      return value.charAt(0).toUpperCase() + (value.slice(1)).toLowerCase();
     }
     return value;
   }
