2f74b303d6069fade0b53e00b5acd9976a567c96
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-08-08 10:48:05 +0300

    site-->accueil  fixed

--- src/app/pages/site/site-accueil-page/site-accueil-page.component.ts
+++ src/app/pages/site/site-accueil-page/site-accueil-page.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {SiteService} from '../../../services/site/site.service';
 import {ErrorMessageHandlerService} from '../../../services/error/error-message-handler.service';
-import {BackendService} from "../../../services/backend/backend.service";
+import {BackendService} from '../../../services/backend/backend.service';
 
 @Component({
-  selector: 'site-accueil-page',
+  selector: 'app-site-accueil-page',
   templateUrl: './site-accueil-page.component.html',
   styleUrls: ['./site-accueil-page.component.css'],
     providers: [SiteService, BackendService]
@@ -27,13 +27,27 @@ export class SiteAccueilPageComponent implements OnInit, OnDestroy {
         this.id_site = localStorage.id_site;
         this.getAccueilInfoFunction();
         this.getFromServerAccueilImageFunction();
-   }
+
+        const refresh = {
+          refresh_token: localStorage.getItem('refresh_token')
+        };
+        this.backendService.token_refresh((refresh))
+          .subscribe(result => {
+            console.log(result);
+            localStorage.setItem('token', result.token);
+            localStorage.setItem('refresh_token', result.refresh_token);
+          }, (err) => {
+            console.log(err);
+          });
+    }
 
     ngOnDestroy() {}
 
   public getAccueilInfoFunction() {
+    this.loading = true;
     this.siteService.getAccueilInfo(this.id_site)
       .subscribe(result => {
+          this.showImg = true;
           this.loading = false;
           this.siteName = result.name;
       }, (err) => {
@@ -46,21 +60,6 @@ export class SiteAccueilPageComponent implements OnInit, OnDestroy {
 
   public getFromServerAccueilImageFunction() {
     this.loading = true;
-    const refresh = {
-      refresh_token: localStorage.getItem('refresh_token')
-    }
-    this.backendService.token_refresh((refresh))
-      .subscribe(result => {
-        console.log(result);
-        localStorage.setItem('token', result.token);
-        localStorage.setItem('refresh_token', result.refresh_token);
-      }, (err) => {
-        console.log(err);
-      });
-  }
-
-
-  public refresh() {
     this.siteService.getFromServerAccueilImage(this.id_site)
       .subscribe(result => {
         const src = 'data:' + result['Content-type'] + ';base64,';
