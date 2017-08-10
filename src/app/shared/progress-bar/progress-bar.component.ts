2c82936233b4dee3ce576936a4f65449fad846b5
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-08-10 16:37:14 +0300

    removed unused

--- src/app/shared/progress-bar/progress-bar.component.ts
+++ src/app/shared/progress-bar/progress-bar.component.ts
@@ -1,49 +1,49 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, OnChanges} from '@angular/core';
 
 @Component({
     selector: 'circle-progress',
     templateUrl: 'progress-bar.component.html',
 })
-export class ProgressBarComponent implements OnInit {
+export class ProgressBarComponent implements OnInit, OnChanges {
 
-    @Input() percent:number = 0;
-    @Input() boxSize:number = 200;
-    @Input() radius:number = 0.9*this.boxSize ;
-    @Input() time:number = 0;
-    @Input() border:number = 20;
-    @Input() color:string = 'none';
-    @Input() backgroundColor:string = 'white';
+    @Input() percent = 0;
+    @Input() boxSize = 200;
+    @Input() radius = 0.9 * this.boxSize ;
+    @Input() time = 0;
+    @Input() border = 20;
+    @Input() color = 'none';
+    @Input() backgroundColor = 'white';
 
-    @Input() lowColor:string = this.color;
-    @Input() middleColor:string = this.color;
-    @Input() interColor:string = this.color;
-    @Input() highColor:string = this.color;
+    @Input() lowColor = this.color;
+    @Input() middleColor = this.color;
+    @Input() interColor = this.color;
+    @Input() highColor = this.color;
 
-    @Input() fontColor:string = 'black';
-    @Input() fontSize: number = 12;
-    @Input() fontFamily:string = 'OpenSans-Semibold';
-    @Input() fontX:string = '50%';
-    @Input() fontY:string = '55%';
+    @Input() fontColor = 'black';
+    @Input() fontSize = 12;
+    @Input() fontFamily = 'OpenSans-Semibold';
+    @Input() fontX = '50%';
+    @Input() fontY = '55%';
 
-    @Input() innerFill:string = "#ffffff";
-    @Input() textAnchor:string = "middle";
+    @Input() innerFill = '#ffffff';
+    @Input() textAnchor = 'middle';
 
-    @Input() caption:string = "your text";
+    @Input() caption = 'your text';
 
 
-    angle:number;
-    radian:number;
-    cx:number;
-    cy:number;
+    angle: number;
+    radian: number;
+    cx: number;
+    cy: number;
 
-    x0:number;
-    y0:number;
-    rx:number;
-    ry:number;
+    x0: number;
+    y0: number;
+    rx: number;
+    ry: number;
 
-    innerRadius:number;
-    // circleText:string = '0%';
-    circleText:string = '0';
+    innerRadius: number;
+    // circleText '0%';
+    circleText = '0';
 
     x;
     y;
@@ -56,7 +56,7 @@ export class ProgressBarComponent implements OnInit {
 
 
 
-    canAnimate:boolean = true;
+    canAnimate = true;
 
     ngOnInit() {
         this.setInputs();
@@ -72,16 +72,16 @@ export class ProgressBarComponent implements OnInit {
 
   public setInputs() {
 
-        if (this.percent <0) {
+        if (this.percent < 0) {
             this.percent *= -1;
         }
 
-        if(this.time == 0){
+        if (this.time === 0) {
             // this.circleText = this.percent+'%';
-            this.circleText = this.percent+'';
+            this.circleText = this.percent + '';
             this.setColor(this.percent);
             this.angle = this.percToAngle(this.percent);
-        }else{
+        }else {
             this.angle = 0;
         }
 
@@ -112,7 +112,7 @@ export class ProgressBarComponent implements OnInit {
         this.x = this.cx + r * Math.sin(rad);
         this.y = this.cy - r * Math.cos(rad);
 
-        if (this.percent == 100) {
+        if (this.percent === 100) {
             this.x--;
         }
 
@@ -138,7 +138,7 @@ export class ProgressBarComponent implements OnInit {
     }
 
   public percToAngle(perc: number) {
-        return perc*3.6;
+        return perc * 3.6;
     }
 
 
@@ -153,7 +153,7 @@ export class ProgressBarComponent implements OnInit {
 
             this.animationLoop(1, time);
 
-        }else{
+        }else {
             return;
         }
 
@@ -161,7 +161,7 @@ export class ProgressBarComponent implements OnInit {
 
 
   public animationLoop(i, time) {
-        setTimeout(()=> {
+        setTimeout(() => {
             this.angle = this.percToAngle(i);
             this.radian = this.angleToRad(this.angle);
             this.setArcSet(this.angle);
@@ -173,23 +173,21 @@ export class ProgressBarComponent implements OnInit {
             if (i <= this.percent) {
                 this.animationLoop(i, time);
             }
-        },time)
-        if(i >= this.percent) {
+        }, time);
+        if (i >= this.percent) {
             this.canAnimate = true;
         }
     }
 
-  public setColor(percent) {
-        if(percent <=25) {
+    public setColor(percent) {
+        if (percent <= 25) {
             this.color = this.lowColor;
-        }else if(percent <=50){
+        }else if (percent <= 50) {
             this.color = this.middleColor;
-        }else if(percent <=75){
+        }else if (percent <= 75) {
             this.color = this.interColor;
-        }else if(percent >75){
+        }else if (percent > 75) {
             this.color = this.highColor;
         }
     }
-
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,11 @@ import {CommonModule} from '@angular/common';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from '../services/routing.module';
-import { LeftMenuComponent } from './left-menu-gena/left-menu.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 import { NavbarAdminComponent } from './navbar/navbar-admin/navbar-admin.component';
 import { NavbarClientComponent } from './navbar/navbar-client/navbar-client.component';
-import { ContentComponent } from './content/content.component';
-import { NavbarForDeleteComponent } from './navbar/navbar-for-delete/navbar-for-delete.component';
 import {BackendService} from '../services/backend/backend.service';
 import {LoginService} from '../services/login/login.service';
-import { ModalStandartComponent, ModalHeader, ModalContent, ModalFooter } from './modal-standart/modal-standart.component';
 import {AdminAccessPipe} from '../services/pipes/accessAdmin.pipe';
 import { NavbarSiteComponent } from './navbar/navbar-site/navbar-site.component';
 import { NavbarSalariesComponent } from './navbar/navbar-salaries/navbar-salaries.component';
@@ -26,25 +22,11 @@ import {DateFromServerMomentPipe} from '../services/pipes/dateFromServerMoment.p
 @NgModule({
     imports: [
         CommonModule, BrowserModule, FormsModule, AppRoutingModule,
-
-      //  BrowserAnimationsModule, // ang-material
-     //   NoopAnimationsModule,  // ang-material
-     //   MdInputModule,   // ang-material
-     //   MdButtonModule,  // ang-material
-     //   MdProgressSpinnerModule,  // ang-material
-        // ModalModule
     ],
     declarations: [
-        LeftMenuComponent,  //моя верстка, удалить потом
         ProgressBarComponent,
         NavbarAdminComponent,
         NavbarClientComponent,
-        ContentComponent,
-
-      //  ButtonTestComponent,  // ang-material
-     //   InputTestComponent, SpinnerComponent,    // ang-material
-        NavbarForDeleteComponent,     //моя верстка, удалить потом
-        ModalStandartComponent, ModalHeader, ModalContent, ModalFooter,
         AdminAccessPipe,
         ValueNullPipe,
         NavbarSiteComponent,
@@ -56,16 +38,9 @@ import {DateFromServerMomentPipe} from '../services/pipes/dateFromServerMoment.p
         DateFromServerMomentPipe
     ],
     exports: [
-        LeftMenuComponent,  //моя верстка, удалить потом
         ProgressBarComponent,
         NavbarAdminComponent,
         NavbarClientComponent,
-        ContentComponent,
-
-      //  ButtonTestComponent,  // ang-material
-      //  InputTestComponent, SpinnerComponent,    // ang-material
-        NavbarForDeleteComponent,    //моя верстка, удалить потом
-        ModalStandartComponent, ModalHeader, ModalContent, ModalFooter,
         AdminAccessPipe,
         ValueNullPipe,
         NavbarSiteComponent,
